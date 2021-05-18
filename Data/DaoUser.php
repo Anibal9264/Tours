a39ce37157596e7c436faa621b729c10774ba7e1
anibal <anibal@anibal-HP-Laptop-14-bw0xx>
2021-05-17 22:51:54 -0600

    update

--- Data/DaoUser.php
+++ Data/DaoUser.php
@@ -10,7 +10,9 @@ class DaoUser {
     }
     
     function Login($email, $password){
-        $sql = "SELECT * from usuario WHERE email = '$email' and contraseña = '$password'";
+        $sql = "SELECT CONCAT(nombre,' ', apellidos) "
+                . "AS nombreC,foto,pais,identificacion,email,fecha_nacimiento "
+                . "FROM usuario WHERE email = '$email' and contraseña = '$password'";
         $sentence = $this->base->query($sql);  
         $result = $sentence->fetch(PDO::FETCH_OBJ);
         return $result;
